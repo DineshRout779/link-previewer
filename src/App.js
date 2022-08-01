941a37c0cd5a48ee46aa463a1d1ab58dd597f425
Dinesh Rout <rddinesh779@gmail.com>
2022-08-01 15:53:46 +0530

    project completed✅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { getLinkPreview } from 'link-preview-js';
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [error, setError] = useState('');
+  const [result, setResult] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setResult(null);
+    setIsLoading(true);
+    setError('');
+
+    if (query) {
+      try {
+        const res = await getLinkPreview(query);
+        setResult(res);
+        setIsLoading(false);
+        setQuery('');
+      } catch (err) {
+        console.log(err);
+        setError(err.message);
+        setIsLoading(false);
+      }
+    } else {
+      setIsLoading(false);
+      setError('Please provide an URL!');
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Link Previewer</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='query'
+          placeholder='Enter an url for preview'
+          className='form-control'
+          value={query}
+          onChange={handleInputChange}
+        />
+
+        <button className='form-control btn-submit' type='submit'>
+          Submit
+        </button>
+      </form>
+
+      <p className='error'>{error ? error : ''}</p>
+
+      <div className='preview'>
+        {isLoading ? (
+          'Loading...'
+        ) : result ? (
+          <div className='preview-card'>
+            <img src={result.images[0]} alt='Link preview' />
+            <div className='content'>
+              <p className='title'>{result.title}</p>
+              <p className='desc'>
+                {result.description.length > 100
+                  ? result.description.slice(0, 100) + '...'
+                  : result.description}
+              </p>
+
+              <a
+                href={result.url}
+                className='url'
+                target='_blank'
+                rel='noreferrer'
+              >
+                Learn more
+              </a>
+            </div>
+          </div>
+        ) : (
+          'Link preview will appear here...✨'
+        )}
+      </div>
     </div>
   );
 }
